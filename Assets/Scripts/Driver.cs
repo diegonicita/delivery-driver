e97fbf1f536ac04e3e085864b55a22e04b203229
diegonicita <diegonicita@gmail.com>
2023-07-04 18:34:39 -0300

    first commit

---
+++ Assets/Scripts/Driver.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    [SerializeField] float steerSpeed = 200;
+    [SerializeField] float moveSpeed = 15;
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveAmount, 0);
+    }
+}
