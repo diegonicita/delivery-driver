a4ce17ec0cb633380b2449ba0d131f7cd6a76027
diegonicita <diegonicita@gmail.com>
2023-07-04 20:38:56 -0300

    Cambio nuevo

--- Assets/Scripts/Collision.cs
+++ Assets/Scripts/Collision.cs
@@ -11,7 +11,7 @@ public class Collision : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("What was that? (CAMBIO)");
+        Debug.Log("What was that? (CAMBIO SOBRE UN CAMBIO)");
     }
 
 
